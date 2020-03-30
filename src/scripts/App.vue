7fb128655aadaf6562abf9f3af39fb425add9acf
tamuraryoya <tamuraryoya@team-lab.com>
2020-03-30 12:22:19 +0900

    ✨ Setup Vue environment

---
+++ src/scripts/App.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  h1 hello you.
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class  extends Vue {
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/scripts/entry.ts
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import App from './App.vue';
+import config from '../config.json';
+
+new Vue({
+  el: '#root',
+  render: (h) => h(App),
+});
