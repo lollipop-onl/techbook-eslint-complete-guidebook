716f6304c1d0006515d0de6f4d07f4924fa45c0d
tamuraryoya <tamuraryoya@team-lab.com>
2020-03-30 15:58:13 +0900

    🔨 実験的な記述を削除

--- src/scripts/Components/AppContainer.vue
+++ src/scripts/Components/AppContainer.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
-.app-container(ref="wrapper" :style="wrapperStyle")
-  .container(ref="container" :style="containerStyle")
+.app-container(ref="wrapper")
+  .container(ref="container")
     slot
 </template>
 
@@ -9,71 +9,7 @@ import { bind, debounce } from 'helpful-decorators';
 import { Component, Ref, Vue } from 'vue-property-decorator';
 
 @Component
-export default class AppContainer extends Vue {
-  /** ラッパの幅 */
-  wrapperWidth = 0;
-
-  /** コンテナの幅 */
-  containerWidth = 0;
-
-  /** コンテナの高さ */
-  containerHeight = 0;
-
-  /** コンテナラッパの要素への参照 */
-  @Ref('wrapper')
-  wrapperElement?: HTMLDivElement;
-
-  /** コンテナの要素への参照 */
-  @Ref('container')
-  containerElement?: HTMLDivElement;
-
-  /** ラッパのスタイル */
-  get wrapperStyle(): Record<string, string> {
-    const scale = this.wrapperWidth / this.containerWidth;
-
-    return {
-      height: `${this.containerHeight * scale}px`,
-    };
-  }
-
-  /** コンテナのスタイル */
-  get containerStyle(): Record<string, string> {
-    const scale = this.wrapperWidth / this.containerWidth;
-
-    return {
-      transform: `scale(${scale})`,
-    };
-  }
-
-  /** ライフサイクル */
-  mounted(): void {
-    window.addEventListener('resize', this.onResize);
-    this.onResize();
-  }
-
-  /** ライフサイクル */
-  beforeDestroy(): void {
-    window.removeEventListener('resize', this.onResize);
-  }
-
-  /** 画面幅が変更されたときに呼ばれるメソッド */
-  @bind
-  @debounce()
-  onResize(): void {
-    const { wrapperElement, containerElement } = this;
-
-    if (!wrapperElement || !containerElement) {
-      return;
-    }
-
-    const { width: wrapperWidth } = wrapperElement.getBoundingClientRect();
-    const { width: containerWidth, height: containerHeight } = containerElement.getBoundingClientRect();
-
-    this.wrapperWidth = wrapperWidth;
-    this.containerWidth = containerWidth;
-    this.containerHeight = containerHeight;
-  }
-}
+export default class AppContainer extends Vue {}
 </script>
 
 <style lang="scss" scoped>
